3cab09d5090bbe25171e408a8a3616f824a120b6
Bala Mahesh <72618397+Bala534@users.noreply.github.com>
2021-07-20 10:55:37 +0530

    Updated

--- main.js
+++ main.js
@@ -4,10 +4,8 @@ This is the main class where we access all other classes through objects
 
 class Start{
     game(){
-        console.log('sjdb')
         var obj = new Main();
         obj.movement();
-        console.log("started")
     }}
 var object = new Start();
 object.game();
